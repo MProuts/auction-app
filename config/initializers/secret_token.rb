d459cb96f340c769d9928bb99455ea1cd6bec0c6
Michael Prouty <michael.prouty@gmail.com>
2014-03-18 19:47:50 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuctionAppSecondGo::Application.config.secret_key_base = '51dbf1ff236484398ce1a8684f668feb97edc0555df00bc0ddd54b8eef8beb9c0a6583b331c0412a7759a7fcc690ebbd038f29d3cabda5dff0aa45fa9b3bde28'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AuctionAppSecondGo::Application.config.session_store :cookie_store, key: '_auction-app-second-go_session'
